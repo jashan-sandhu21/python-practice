1f372555c3cc476a993e6f6be090ff66407cb832
jashan-sandhu21 <jashansd21@gmail.com>
2026-03-26 00:12:36 +0000

    calculating your life in weeks

---
+++ week1/your_life_in_weeks.py
@@ -0,0 +1,6 @@
+Age = int(input("Enter your current age:"))
+years_remaining = 90 - Age
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+print(f"you have {days_remaining} days, {weeks_remaining} weeks, {months_remaining} months left ")
