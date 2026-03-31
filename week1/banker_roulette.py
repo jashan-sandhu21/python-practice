c0d67f7436355494a385a44a9ed7ff91de359916
jashan-sandhu21 <jashansd21@gmail.com>
2026-03-31 04:47:59 +0000

    random name generator

---
+++ week1/banker_roulette.py
@@ -0,0 +1,8 @@
+import random
+names = input("Give me everybody's names separated by commas.")
+name = names.split(",")
+print(name)
+total_persons = len(name)
+bill = random.randint(0,total_persons-1)
+person_who_will_pay = name[bill]
+print(person_who_will_pay+  "will pay the bill. ")
